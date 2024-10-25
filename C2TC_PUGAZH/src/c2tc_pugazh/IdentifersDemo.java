67a3d491d2a5be4e4a5c088dc15bea46f49c22ed
ASUS <ASUS@Pugazh>
2024-10-25 14:49:21 +0530

    Identifers Demo

---
+++ C2TC_PUGAZH/src/c2tc_pugazh/IdentifersDemo.java
@@ -0,0 +1,13 @@
+package c2tc_pugazh;
+
+public class IdentifersDemo {
+
+	public static void main(String[] args) {
+		int num=107;
+		System.out.println("Value of the number varible is: "+ num);
+		String studname="Pugazh";
+		System.out.println("Name: "+ studname);
+
+	}
+
+}
