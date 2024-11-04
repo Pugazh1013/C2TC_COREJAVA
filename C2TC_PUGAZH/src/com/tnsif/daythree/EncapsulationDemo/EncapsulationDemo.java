2e6c0aac9e0b009e6e3f639e88670686d7ad07db
ASUS <ASUS@Pugazh>
2024-11-04 15:50:10 +0530

    Day3

---
+++ C2TC_PUGAZH/src/com/tnsif/daythree/EncapsulationDemo/EncapsulationDemo.java
@@ -0,0 +1,14 @@
+package com.tnsif.daythree.EncapsulationDemo;
+
+public class EncapsulationDemo {
+
+	public static void main(String[] args) {
+		OopsDemo obj=new OopsDemo();
+		obj.setSerialNum(101);
+		obj.setName("Pooja");
+		obj.setAge(20);
+		System.out.println(obj);
+
+	}
+
+}
---
+++ C2TC_PUGAZH/src/com/tnsif/daythree/ScannerDemo/Person.java
@@ -0,0 +1,48 @@
+package com.tnsif.daythree.ScannerDemo;
+
+public class Person {
+		private String Name;
+		private double Income;
+		private String Gender;
+		private int Age;
+		private double Tax;
+		public String getName() {
+			return Name;
+		}
+		public void setName(String name) {
+			Name = name;
+		}
+		public double getIncome() {
+			return Income;
+		}
+		public void setIncome(double income) {
+			Income = income;
+		}
+		public String getGender() {
+			return Gender;
+		}
+		public void setGender(String gender) {
+			Gender = gender;
+		}
+		public int getAge() {
+			return Age;
+		}
+		public void setAge(int age) {
+			Age = age;
+		}
+		public double getTax() {
+			return Tax;
+		}
+		public void setTax(double tax) {
+			Tax = tax;
+		}
+		@Override
+		public String toString() {
+			return "Person [Name=" + Name + ", Income=" + Income + ", Gender=" + Gender + ", Age=" + Age + ", Tax="
+					+ Tax + "]";
+		}
+		
+		
+		
+
+}
