c7a77d3ed53c439f9f7679630d2f86f6992c6aba
ASUS <ASUS@Pugazh>
2024-11-04 15:36:20 +0530

    Day3

---
+++ C2TC_PUGAZH/src/com/tnsif/daythree/Encapsulation/OopsDemo.java
@@ -0,0 +1,31 @@
+package com.tnsif.daythree.Encapsulation;
+
+public class OopsDemo {
+	private int SerialNum;
+	private String Name;
+	private int Age;
+	public int getSerialNum() {
+		return SerialNum;
+	}
+	public void setSerialNum(int serialNum) {
+		SerialNum = serialNum;
+	}
+	public String getName() {
+		return Name;
+	}
+	public void setName(String name) {
+		Name = name;
+	}
+	public int getAge() {
+		return Age;
+	}
+	public void setAge(int age) {
+		Age = age;
+	}
+	@Override
+	public String toString() {
+		return "OopsDemo [SerialNum=" + SerialNum + ", Name=" + Name + ", Age=" + Age + "]";
+	}
+	
+
+}
