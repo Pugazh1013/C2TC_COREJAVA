474486f679f80308952bec58fad06a261ceb81e2
aarth <aarth@LAPTOP-LMQ0QGDL>
2024-11-08 07:20:19 +0530

    commit

---
+++ C2TC_Aarthi/src/com/tns/ifet/day3/singleinheritance/SlInheritanceDemo.java
@@ -0,0 +1,12 @@
+package com.tns.ifet.day3.singleinheritance;
+
+public class SlInheritanceDemo {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Student student = new Student("Gayatri", "8080808080", "Bangalore", 9888878787L, 10, "City College"); 
+		System.out.println(student);
+
+	}
+
+}
