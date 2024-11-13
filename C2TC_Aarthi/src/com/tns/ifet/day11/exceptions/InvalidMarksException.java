8d4dd8e15e3b6f877f45366409ccddbe185273fd
aarth <aarth@LAPTOP-LMQ0QGDL>
2024-11-13 07:32:44 +0530

    commit

---
+++ C2TC_Aarthi/src/com/tns/ifet/day11/exceptions/InvalidMarksException.java
@@ -0,0 +1,12 @@
+package com.tns.ifet.day11.exceptions;
+
+public class InvalidMarksException extends Exception {
+	public InvalidMarksException() {
+		super();
+	}
+
+	public InvalidMarksException(String message) {
+		super(message);
+	}
+
+}
