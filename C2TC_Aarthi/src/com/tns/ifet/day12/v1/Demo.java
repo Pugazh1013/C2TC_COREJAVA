77274693818c98e774aa68eadafabea6f9ea4cf7
aarth <aarth@LAPTOP-LMQ0QGDL>
2024-11-13 07:39:31 +0530

    commit

---
+++ C2TC_Aarthi/src/com/tns/ifet/day12/v1/Demo.java
@@ -0,0 +1,11 @@
+package com.tns.ifet.day12.v1;
+
+public class Demo {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("the program continues....");
+		int data=100/0;
+	}
+
+}
